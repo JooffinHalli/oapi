9820f60198acf96d4bed40581c678a249a7d9804
Бойко Артем <boiko.a@MacBook-Pro-Bojko.local>
2024-08-23 20:06:39 +0300

    delete types

--- types/utilTypes/index.ts
+++
@@ -1,6 +0,0 @@
-type Utils<L, C extends any[] = [], R = L> = 
-  C['length'] extends L
-    ? R
-    : Utils<L, [...C, 0], C['length'] | R>
-
-export type NumberRange<L, H> = L | Exclude<Utils<H>, Utils<L>>
