9b00130f945cccc5e8705c7b2f5d30d9160b9b3e
Бойко Артем <boiko.a@MacBook-Pro-Bojko.local>
2024-11-29 03:00:55 +0300

    comm

--- src/strings.js
+++ src/strings.js
@@ -11,6 +11,7 @@ var jsDoc = (url, info) => (
   `\n * @see {@link ${url} swagger}` +
   (info?.description ? `\n * @description ${info?.description}` : '') +
   (info?.summary ? `\n * @summary ${info?.summary}` : '') +
+  (info?.title ? `\n * @title ${info?.title}` : '') +
   '\n */\n'
 );
 
