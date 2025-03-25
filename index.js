66eca21d99636682802d43ce970c1cf9e01be32c
Бойко Артем <boiko.a@MacBook-Pro-Bojko.local>
2025-03-25 20:43:50 +0300

    comm

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ process.on('uncaughtException', (e) => {
   console.log('\x1b[31m%s\x1b[0m', `\nпроизошла ошибка, проверьте входные данные\n`); // red
 });
 
-var basePath = process.cwd();
-
 var path = require('node:path');
 
+var configPath;
 var config = configFilePathOrUrl.startsWith('http')
   ? [{ url: configFilePathOrUrl, output: 'TEST_API_FOLDER/test-service' }]
-  : require(path.join(basePath, path.normalize(configFilePathOrUrl)));
+  : require(configPath = path.join(process.cwd(), path.normalize(configFilePathOrUrl)));
 
 var fetchAndPrint = require('./src/io');
 
-config.forEach(fetchAndPrint(basePath));
+config.forEach(fetchAndPrint(configPath ? path.dirname(configPath) : configPath));
--- src/io.js
+++ src/io.js
@@ -1,44 +1,51 @@
 var fs = require('node:fs');
+var child_process = require('node:child_process');
 var path = require('node:path');
 var OpenapiTranspiler = require('./transpiler');
-var strings = require('./strings');
+var { banner, jsDoc, imports } = require('./strings');
 
-var fetchAndPrint = (basePath) => (config) => {
+var fetchAndPrint = (configPath) => (config) => {
 
   var gen = config.generate;
-  var output = config.output ? path.join(basePath, path.normalize(config.output)) : '_API_FOLDER_';
+  var output = config.output ? path.join(configPath, path.normalize(config.output)) : '_API_FOLDER_';
   var url = config.url;
-  var hook = config.hook ? require(path.join(basePath, path.normalize(config.hook))) : (() => {});
+  var hook = config.hook ? require(path.join(configPath, path.normalize(config.hook))) : (() => { });
+
+  var handleDoc = (openapiDoc) => {
+    if (!openapiDoc) return;
+
+    var transpiler = new OpenapiTranspiler(openapiDoc, config, log, hook);
+    var program = OpenapiTranspiler.prepare(openapiDoc);
+
+    var { schemas, paths } = transpiler.reduce(program,
+      {
+        schemas: (gen?.schemas === false) ? false : '',
+        paths: (gen?.paths === false) ? false : ''
+      }
+    );
+
+    var fn = () => {};
+    var comment = jsDoc(url, openapiDoc.info);
+
+    (schemas || paths) && fs.mkdirSync(output, { recursive: true });
+    schemas && fs.writeFile(output + '/schemas.ts', banner + comment + schemas, null, fn);
+    paths && fs.writeFile(output + '/paths.ts', banner + imports + comment + paths, null, fn);
+  }
 
   var log = (msg) => {
     console.log('\x1b[31m%s\x1b[0m', `\n-------\nurl - ${url}\n${msg}\n-------\n`);
   }
 
-  fetch(url)
+  fetch(url, { headers: { 'Accept': 'application/json' } })
     .then((r) => r.json())
-    .catch(() => log('произошла ошибка при запросе по сети'))
-    .then((openapiDoc) => {
-      if (!openapiDoc) return;
-
-      var transpiler = new OpenapiTranspiler(openapiDoc, config, log, hook);
-
-      var { schemas, paths } = transpiler.reduce(
-        OpenapiTranspiler.prepare(openapiDoc),
-        {
-          schemas: (gen?.schemas === false) ? false : '',
-          paths:   (gen?.paths === false)   ? false : ''
-        }
-      );
-
-      var { banner, jsDoc, imports } = strings;
-      var fn = () => {};
-      var comment = jsDoc(url, openapiDoc.info);
-
-      (schemas || paths) && fs.mkdirSync(output, { recursive: true });
-      schemas && fs.writeFile(output + '/schemas.ts', banner + comment + schemas, null, fn);
-      paths && fs.writeFile(output + '/paths.ts', banner + imports + comment + paths, null, fn);
+    .then(handleDoc)
+    .catch(() => {
+      child_process.exec(`curl -v ${url}`, (err, res) => {
+        if (err) log('произошла ошибка при запросе по сети');
+        else handleDoc(JSON.parse(res));
+      });
     })
-    .catch(() => log('произошла ошибка, проверьте входные данные'));
+    .catch((e) => log(e, 'произошла ошибка, проверьте входные данные'));
 
 }
 
