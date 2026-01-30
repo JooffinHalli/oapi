c5a2bb30aae9b5036d2201c7037d33c08fc9760e
Бойко Артем <boiko.a@MacBook-Pro-Bojko.local>
2026-01-30 20:10:25 +0300

    comm

--- swaggerToTS.js
+++ swaggerToTS.js
@@ -62,6 +62,9 @@ var alphabet = {
     '@anyProperty'(schema, name) {
         var types = runSchema.call(this, schema);
         var jsdoc = runComment.call(this, schema);
+        if ('legal_address' === name) {
+            console.log(name, types, schema);
+        }
         return `${jsdoc}${name.toKey(!schema.isRequired).toCamelCase(this.toCamelCase)}: ${types}`;
     },
     'items'(schema) {
@@ -159,6 +162,7 @@ function runComment(schema, extraRow = '') {
 function runComposition(devider) {
     return function(composition) {
         var res = composition.map(runSchema, this).join2(devider);
+        console.log(composition, res);
         return res;
     };
 }
